55a15693c2b4f4d5d9a551a47c93f5ccfa30dcad
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.35>
2024-01-20 09:53:14 +0700

    New Update

--- lib/locomotive/coal/resources/content_assets.rb
+++ lib/locomotive/coal/resources/content_assets.rb
@@ -2,7 +2,30 @@ module Locomotive::Coal
   module Resources
 
     class ContentAssets < Base
+      def index(query = nil, options = {}, locale = nil)
+        parameters = { where: (query || {}).to_json }.merge(options || {})
+        
+        response = get(resources_name, parameters, true)
+        response = get(resources_name, parameters, true)
+        
+        list = response.body.map { |attributes| Resource.new(attributes) }
 
+        PaginatedResources.new(list,
+          options[:page] || 1,
+          response.headers[:x_total_pages].to_i,
+          response.headers[:x_total_entries].to_i)
+      end
+
+      def each(query = nil, options = nil, locale = nil, &block)
+        page = 1
+        while page do
+          resources = all(query, (options || {}).merge(page: page), locale)
+          resources.each(&block)
+          page = resources._next_page
+        end
+      end
+
+      alias :all :index
     end
 
   end
